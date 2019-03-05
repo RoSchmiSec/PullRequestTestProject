cb0fb2497b70b7b4bea97132302a97cfe43ea9af
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2019-03-05 16:47:13 +0100

    Added line in Program.cs

--- PullRequestTestProject/Program.cs
+++ PullRequestTestProject/Program.cs
@@ -7,6 +7,7 @@ namespace PullRequestTestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Github!");
             Console.ReadKey();
         }
     }
